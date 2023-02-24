b6fb60752f309e70fa5862b31d0781d2fc419cba
Ashique Arman <26833749+AshiqueArman@users.noreply.github.com>
2023-02-24 03:12:14 -0400

    Add files via upload
    designed the filter option

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
+
 //event listeners
 
 todoButton.addEventListener('click', addTodo);
